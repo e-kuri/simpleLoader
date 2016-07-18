9ab7536ef5b9e4bde436cb195a7691f466ec5261
e-kuri <eugenio.kuri@gmail.com>
2016-07-18 12:16:24 -0400

    permissions asked

--- app/src/main/java/com/example/simpleloader/MainActivity.java
+++ app/src/main/java/com/example/simpleloader/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.simpleloader;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
@@ -17,22 +23,44 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private static final int LIST_ID = 10;
 
+    private static final int SMS_CALL_LOG = 69;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getSupportLoaderManager().initLoader(LIST_ID, null, this);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_SMS)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, SMS_CALL_LOG);
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS}, SMS_CALL_LOG);
+            }
+        }else {
+            getSupportLoaderManager().initLoader(LIST_ID, null, this);
+        }
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(getApplicationContext(),
-                Uri.parse(SMS_CONTENT_URI),
-                null,
-                null,
-                null,
-                null);
+
+            return new CursorLoader(getApplicationContext(),
+                    Uri.parse(SMS_CONTENT_URI),
+                    null,
+                    null,
+                    null,
+                    null);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == SMS_CALL_LOG){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getSupportLoaderManager().initLoader(LIST_ID, null, this);
+            }else{
+                Toast.makeText(this, "no use for this app", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
